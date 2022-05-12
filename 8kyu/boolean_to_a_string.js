6e15632a06013c62a43dace9ed5f88afaca3d43a
Victor <64720348+Gitpiff@users.noreply.github.com>
2022-05-12 18:20:30 -0500

    boolean_to_a_string

---
+++ 8kyu/boolean_to_a_string.js
@@ -0,0 +1,9 @@
+//Convert a Boolean to a String
+//Solution 1
+function booleanToString(b){
+    //your code here
+    return b ? "true" : "false" //Use a ternary to return "true" or "false"
+}
+
+//Solution 2 -one line-
+const booleanToStrin = (b) => b ? "true" : "false"
