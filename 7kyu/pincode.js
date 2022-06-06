6df9898d65ea24ab17f5cac7e15d9f187beb2318
Victor <64720348+Gitpiff@users.noreply.github.com>
2022-06-05 20:23:26 -0500

    Update pincode.js

--- 7kyu/pincode.js
+++ 7kyu/pincode.js
@@ -2,10 +2,22 @@
 
 // If the function is passed a valid PIN string, return true, else return false.
 
+/* for my future referencing,
+
+  ^ = start of a line, 
+  \d = [0-9], {4} specifies 4 digits, 
+  {6} specifies 6 digits, $ - end of a line
+  
+  first alternative ^\d{4}$ - equivalent to [0-9], matches exactly 4 digits.
+  second alternative ^\d{6}$ - "", matches previous token exactly 6 digits.
+  
+   use RegEx .test to search for these characters stored.
+   
+*/ 
+
 function validatePIN (pin) {
     //return true or false
-    return pin.length === 4 || pin.length  === 6 ? "true" : "false" 
-
+    return /^(\d{4}|\d{6})$/.test(pin)
 }
 
-console.log(validatePIN("1"))
+console.log(validatePIN("123e"))
