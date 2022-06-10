d015314bf8450e9d309c4746b8c69b8c330c2194
Victor <64720348+Gitpiff@users.noreply.github.com>
2022-06-10 09:50:57 -0500

    Create fiveSort.js

---
+++ structy/fiveSort.js
@@ -0,0 +1,10 @@
+//Write a function, "fiveSort", that takes in an array of numbers as an argument. The function should rearrange elements of the
+//array such that all 5s appear at the end. Your function should perform this operation in-place by mutating the original
+//array. The function should return an array. Elements that are not 5 can appear in any order in the output, as long as
+//all 5s are at the end of the array.
+
+const fiveSort = (nums) => {
+
+}
+
+console.log(fiveSort([1,4,5,6,5,7,5,8,5]))
