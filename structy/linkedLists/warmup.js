27560d3c2e6b0b7c9dd3cd6567f4419c0c2c5fa9
Victor <64720348+Gitpiff@users.noreply.github.com>
2022-06-11 19:13:36 -0500

    Create warmup.js

---
+++ structy/linkedLists/warmup.js
@@ -0,0 +1,11 @@
+class Node {
+    constructor(val) {
+        this.value = val;
+        this.next = null;
+    }
+}
+const a = new Node ("A");
+const b = new Node ("B");
+const c = new Node ("C");
+const d = new Node ("D");
+const e = new Node ("E");
