4c8d108b283dd2b8fa1a26f2cad41bac03da0362
Victor <viknavarro@gmail.com>
2022-06-12 20:41:43 -0500

    Create sumList.js

---
+++ structy/linkedLists/sumList.js
@@ -0,0 +1,10 @@
+//Write a function, "sumList", that takes in the head of a linked list containing numbers as an argument, The function should return the total sum of all values in the linked list.
+
+
+
+const sumList = (head) => {
+
+}
+
+
+console.log(sumList())
