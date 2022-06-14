3dc209b3ab4045a37e70c09cd091eb4a5d4add08
Victor <64720348+Gitpiff@users.noreply.github.com>
2022-06-13 19:40:56 -0500

    Create listValue.js

---
+++ structy/linkedLists/listValue.js
@@ -0,0 +1,8 @@
+//Write a function, "linkedListValues" that takes in the head of a linked list as an argument, 
+//the function should return an array containing all values of the nodes in the linked list
+
+const linkedListValues = (head) => {
+
+}
+
+console.log(linkedListValues)
