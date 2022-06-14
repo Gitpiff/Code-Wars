134204bbefa102445d11754e6a794cfba3661f43
Victor <viknavarro@gmail.com>
2022-06-14 15:51:20 -0500

    updated sumList

--- structy/linkedLists/listValue.js
+++ structy/linkedLists/listValue.js
@@ -1,31 +1,53 @@
-//Write a function, "linkedListValues" that takes in the head of a linked list as an argument, 
+//Write a function, "linkedListValues" that takes in the head of a linked list as an argument,
 //the function should return an array containing all values of the nodes in the linked list
 
+class Node {
+    constructor(val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+
+const a = new Node ("A");
+const b = new Node ("B");
+const c = new Node ("C");
+const d = new Node ("D");
+const e = new Node ("E");
+
+a.next = b;
+b.next = c;
+c.next = d;
+d.next = e;
 
 //Iterative Solution
-const linkedListValues = (head) => {
-    const values = [];
-    let current = head;
-    while(current !== head) {
-        values.push(current.val);
-        current = current.next;
-    }
-
-    return values
-}
+// const linkedListValues = (head) => {
+//     //declare a variable containing the array expected as the result
+//     const values = [];
+//     //use a pointer to traverse in order
+//     let current = head;
+//     //check conditional
+//     while(current !== null) {
+//         //if conditional is met, push value to array
+//         values.push(current.val);
+//         //move the "pointer" to the next value
+//         current = current.next;
+//     }
+//     //
+//     return values
+// }
 
 
 //Recursive Solution
 const linkedListValues = (head) => {
     const values = [];
-    _linkedListValues(head, values);
+    fillValues(head, values);
     return values;
 }
 
-const linkedListValues = (head, values) => {
+const fillValues = (head, values) => {
     if(head === null) return;
     values.push(head.val);
-    _linkedListValues(head.next, values)
+    fillValues(head.next, values)
 }
 
 console.log(linkedListValues)
--- structy/linkedLists/sumList.js
+++ structy/linkedLists/sumList.js
@@ -1,30 +1,54 @@
 //Write a function, "sumList", that takes in the head of a linked list containing numbers as an argument, The function should return the total sum of all values in the linked list.
 
-
-//Iterative
-const sumList = (head) => {
-    let sum = 0;
-    let current = head;
-    while(current !== null) {
-        sum += current.val;
+class Node {
+    constructor(val) {
+        this.val = val;
+        this.next = null;
     }
-
-    return sum
 }
 
-//n = number of nodes
-//Time: O(n)
-//Space: O(1)
+const a = new Node (2);
+const b = new Node (8);
+const c = new Node (3);
+const d = new Node (-1);
+const e = new Node (7);
+
+a.next = b;
+b.next = c;
+c.next = d;
+d.next = e;
 
 
-//Recursive
+
+//Iterative
+// const sumList = (head) => {
+//     //declare a variable result, initial value null
+//     let result = 0;
+//     //pointer to get each node's value
+//     let current = head;
+//     //add value to the result variable
+//     //iterate using a while loop
+//     while(current !== null) {
+//       result += current.val;
+//       current = current.next
+//     }
+
+//     return result
+//   };
+
+// //n = number of nodes
+// //Time: O(n)
+// //Space: O(1)
+
+
+// //Recursive
 const sumList = (head) => {
     if(head === null) return 0;
     return head.val + sumList(head.next);
 }
 
-//n = number of nodes
-//Time: O(n)
-//Space: O(n)
+// //n = number of nodes
+// //Time: O(n)
+// //Space: O(n)
 
-console.log(sumList())
+console.log(sumList(a))
