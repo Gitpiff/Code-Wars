25416c0e34fa896338652a25bf6c6f266ed04c04
Victor <64720348+Gitpiff@users.noreply.github.com>
2022-06-16 18:30:47 -0500

    Update mergeLists.js

--- structy/linkedLists/mergeLists.js
+++ structy/linkedLists/mergeLists.js
@@ -33,6 +33,27 @@ x.next = y;
 
 
 const mergeLists = (head1, head2) => {
+    let dummyHead = new Node(null);
+    let tail = dummyHead;
+    let current1 = head1;
+    let current2 = head2;
+
+    while (current1 !== null && current2 !== null) {
+        if (current1.val < current2.val) {
+            tail.next = current1;
+            current1 = current1.next;
+        } else {
+            tail.next = current2;
+            current2 = current2.next;
+        }
+
+        tail = tail.next
+    }
+
+    if (current1 !== null) tail.next = current1;
+    if (current2 !== null) tail.next = current2;
+
+    return dummyHead.next
 
 }
 
