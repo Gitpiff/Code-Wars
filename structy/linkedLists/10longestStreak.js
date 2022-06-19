ece965834afa0fb878f9ac9960c765e5ae80d72c
Victor <viknavarro@gmail.com>
2022-06-19 15:49:05 -0500

    updateLongestStreak

--- structy/linkedLists/10longestStreak.js
+++ structy/linkedLists/10longestStreak.js
@@ -9,9 +9,9 @@ class Node {
 }
 const a = new Node ("A");
 const b = new Node ("A");
-const c = new Node ("A");
-const d = new Node ("A");
-const e = new Node ("A");
+const c = new Node ("C");
+const d = new Node ("C");
+const e = new Node ("C");
 
 a.next = b;
 b.next = c;
@@ -19,21 +19,30 @@ c.next = d;
 d.next = e;
 
 const longestStreak = (head) => {
-    //Declare a current pointer
+    //Declare a Max streak variable
+    let maxStreak = 0;
+    //Declare a current streak var
+    let currentStreak = 0;
+    //check for values that are the same, declare a var that will store the previous value, declare it as an empty var
+    let prevVal = "";
+    // //Declare a pointer that will point at the current head of the list
     let current = head;
-    //Declare a count
-    let count = 0;
-    //Iterate -while loop-
-    while (current !== null) {
-        if(current.val === head.val) {
-            count += 1;
-        }
-        current = current.next;
-    }
-    //conditional, if current.val === head.val count += 1
-    //else return count
 
-    return count
+    //Iterate -while loop-, assign the first value of the list's head to be the value of prevVal
+    while (current !== null) {
+        prevVal = current.val;
+        if(head.val === prevVal) {
+            currentStreak += 1;
+        }
+        current = current.next
+    }
+
+    if (currentStreak > maxStreak) {
+        maxStreak = currentStreak
+    }
+    //add 1 to the current-streak, also we need a conditional if the current streak is greater than the max-streak then we'll reassign the value of the current streak to the max streak
+
+    return maxStreak
 }
 
 console.log(longestStreak(a))
