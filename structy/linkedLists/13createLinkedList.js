260dfa5cb90e2940da470ba1895e1126e636fb82
Victor <viknavarro@gmail.com>
2022-06-21 20:26:41 -0500

    Update 13createLinkedList.js

--- structy/linkedLists/13createLinkedList.js
+++ structy/linkedLists/13createLinkedList.js
@@ -7,8 +7,40 @@ class Node {
     }
 }
 
-const createLinkedList = (values) => {
-    
+//Iterative
+// const createLinkedList = (values) => {
+//     //use a dummy head
+//     const dummyHead = new Node(null)
+//     //create a tail pointer pointing to the dummy head
+//     let tail = dummyHead;
+//     //create a current pointer, iterating through the array using a for of loop
+//     for (let val of values) {
+//         //given an element of the array create a new node containing that element as a value and reassign the pointer of the dummy head to tail.next, linking the dummy head to the newly created first node
+//         tail.next = new Node(val);
+//         tail = tail.next
+//     }
+
+
+//     return dummyHead.next
+
+// }
+
+//Recursion 1
+// const createLinkedList = (values) => {
+//     if (values.length === 0) return null;
+//     const head =  new Node(values[0]);
+//     head.next = createLinkedList(values.slice(1));
+//     return head;
+// }
+
+//Recursion 2
+const createLinkedList = (values, i = 0) => {
+    if (i === values.length) return null;
+    const head =  new Node(values[i]);
+    head.next = createLinkedList(values, i + 1);
+    return head;
 }
+
+
 
 console.log(createLinkedList([1, 7, 1, 8]))
