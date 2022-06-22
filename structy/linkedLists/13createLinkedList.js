92f90d23a64b5859eebbe7693d547a2cc85ab086
Victor <viknavarro@gmail.com>
2022-06-21 19:59:53 -0500

    Create 13createLinkedList.js

---
+++ structy/linkedLists/13createLinkedList.js
@@ -0,0 +1,14 @@
+//Write a function, "createLinkedList", that takes in an array of values as an argument. The function should create a linked lost containing each element of the array as the values of the nodes. the function should return the head if the linked list.
+
+class Node {
+    constructor(val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+
+const createLinkedList = (values) => {
+    
+}
+
+console.log(createLinkedList([1, 7, 1, 8]))
