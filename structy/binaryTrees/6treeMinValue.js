4af111b03fbf3db8a4365dab812ad413132bc17d
Victor <viknavarro@gmail.com>
2022-06-24 17:10:35 -0500

    Update 6treeMinValue.js

--- structy/binaryTrees/6treeMinValue.js
+++ structy/binaryTrees/6treeMinValue.js
@@ -21,8 +21,58 @@ b.left = d;
 b.right = e;
 c.right = f;
 
-const treeMinValue = (root) => {
+//Solution 1, Iterative Depth First
+// const treeMinValue = (root) => {
+//     //check if tree is empty
+//     if (root === null) return null;
+//     //declare stack
+//     const stack = [root];
+//     //start a minimum var, and replace it every time the new value is smaller than this var, start it at infinity because it'll be guaranteed that any number we have will be smaller than our var
+//     let smallest = Infinity;
+//     //iterate
+//     while (stack.length > 0) {
+//         //create a current var to track the current element
+//         const current = stack.pop();
+//         if (current.val < smallest) smallest = current.val;
+//         //check if children nodes exist
+//         if(current.left) queue.push(current.left);
+//         if(current.right) queue.push(current.right);
+//     }
+//     return smallest;
+// }
 
+
+//Solution 2, Iterative Breadth First
+// const treeMinValue = (root) => {
+//     //check if tree is empty
+//     if (root === null) return null;
+//     //declare queue
+//     const queue = [root];
+//     //start a minimum var, and replace it every time the new value is smaller than this var, start it at infinity because it'll be guaranteed that any number we have will be smaller than our var
+//     let smallest = Infinity;
+//     //iterate
+//     while (queue.length > 0) {
+//         //create a current var to track the current element
+//         const current = queue.shift();
+//         if (current.val < smallest) smallest = current.val;
+//         //check if children nodes exist
+//         if(current.left) queue.push(current.left);
+//         if(current.right) queue.push(current.right);
+//     }
+//     return smallest;
+// }
+
+//Recursive
+const treeMinValue = (root) => {
+    if (root === null) return Infinity;
+
+    //get the children nodes
+    const leftMin = treeMinValue(root.left);
+    const rightMin = treeMinValue(root.right);
+    //use Math.min() to return the smallest num between the three arguments passed
+    return Math.min(root.val, leftMin, rightMin);
 }
+
+
 
 console.log(treeMinValue(a))
