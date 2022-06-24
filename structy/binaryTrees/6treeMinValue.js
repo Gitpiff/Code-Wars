ad5c0bc906e1d491b7146368b610803f29233a88
Victor <viknavarro@gmail.com>
2022-06-24 16:36:06 -0500

    Create 6treeMinValue.js

---
+++ structy/binaryTrees/6treeMinValue.js
@@ -0,0 +1,28 @@
+//Write a function, "treeMinValue", that takes in the root of a binary tree that contains number values. The function should return the minimum value within the tree.
+
+class Node {
+    constructor(val) {
+        this.val = val;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+const a = new Node(3);
+const b = new Node(11);
+const c = new Node(4);
+const d = new Node(4);
+const e = new Node(-2);
+const f = new Node(1);
+
+a.left = b;
+a.right = c;
+b.left = d;
+b.right = e;
+c.right = f;
+
+const treeMinValue = (root) => {
+
+}
+
+console.log(treeMinValue(a))
