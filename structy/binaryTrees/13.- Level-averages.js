4cb56f921de06dbec6744cf86086908941bc57b3
Victor <viknavarro@gmail.com>
2022-06-30 15:24:36 -0500

    Create 13.- Level-averages.js

---
+++ structy/binaryTrees/13.- Level-averages.js
@@ -0,0 +1,34 @@
+//Write a function, "levelAverages", that takes in the root of a binary tree that contains number values. The function should return an array containing the average value of each level.
+
+class Node {
+    constructor(val) {
+        this.val = val;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+const a = new Node(3);
+const b = new Node(11);
+const c = new Node(4);
+const d = new Node(4);
+const e = new Node(-2);
+const f = new Node(1);
+
+a.left = b;
+a.right = c;
+b.left = d;
+b.right = e;
+c.right = f;
+
+//       3
+//    /    \
+//   11     4
+//  / \      \
+// 4   -2     1
+
+const levelAverages = (root) => {
+
+}
+
+console.log(levelAverages(a))
