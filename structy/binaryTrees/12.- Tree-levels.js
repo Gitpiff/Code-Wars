e1afe9d967d8af8e99928d0120b6c5ffa964a1a8
Victor <viknavarro@gmail.com>
2022-06-30 15:14:45 -0500

    Update 12.- Tree-levels.js

--- structy/binaryTrees/12.- Tree-levels.js
+++ structy/binaryTrees/12.- Tree-levels.js
@@ -1,5 +1,4 @@
-//Write a function. "treeLevels", that takes in the root of a binary tree. The function should return a 2-Dimensional array
-//where each subarray represents a level of the tree.
+//Write a function. "treeLevels", that takes in the root of a binary tree. The function should return a 2-Dimensional array where each subarray represents a level of the tree.
 
 class Node {
     constructor(val) {
@@ -30,15 +29,17 @@ c.right = f;
 
 //Solution 1 breadth first (iterative)
 // const treeLevels = (root) => {
+//   //if tree is empty
 //   if (root === null) return [];
 
 //   const levels = [];
+//   //create a stack frame -includes the node itself and the level-
 //   const queue = [{ node: root, levelNum: 0 }];
 //   while (queue.length > 0) {
 //     const { node, levelNum } = queue.shift();
 
 //     if (levels.length === levelNum) {
-//       levels[levelNum] = [node.val];
+//       levels.push([node.val]);
 //     } else {
 //       levels[levelNum].push(node.val);
 //     }
@@ -56,16 +57,17 @@ const treeLevels = (root) => {
     _treeLevels(root, levels, 0);
     return levels;
   };
-  
+
+  //helper function to drive the recursion
   const _treeLevels = (root, levels, levelNum) => {
     if (root === null) return;
-  
+
     if (levels.length === levelNum) {
       levels[levelNum] = [root.val];
     } else {
       levels[levelNum].push(root.val);
     }
-  
+
     _treeLevels(root.left, levels, levelNum + 1);
     _treeLevels(root.right, levels, levelNum + 1);
   };
