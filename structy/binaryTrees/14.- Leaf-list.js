0d7511830e5dc8c52c3444c47e729f8b8e73fae1
Victor <64720348+Gitpiff@users.noreply.github.com>
2022-07-01 20:25:11 -0500

    Create 14.- Leaf-list.js

---
+++ structy/binaryTrees/14.- Leaf-list.js
@@ -0,0 +1,35 @@
+//Write a function, "leafList", that takes in the root of a binary tree, and returns an array containing the values of
+//all leaf nodes in left to right order
+
+class Node {
+    constructor(val) {
+        this.val = val;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+const a = new Node("a");
+const b = new Node("b");
+const c = new Node("c");
+const d = new Node("d");
+const e = new Node("e");
+const f = new Node("f");
+
+a.left = b;
+a.right = c;
+b.left = d;
+b.right = e;
+c.right = f;
+
+//      a
+//    /   \
+//   b     c
+//  / \     \
+// d   e     f
+
+const leafList = (root) => {
+
+};
+
+console.log(leafList(a));
