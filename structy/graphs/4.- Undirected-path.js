5683ad1b13152a41ccb12572803f955654ce304d
Victor <64720348+Gitpiff@users.noreply.github.com>
2022-07-02 20:22:44 -0500

    Create 4.- Undirected-path.js

---
+++ structy/graphs/4.- Undirected-path.js
@@ -0,0 +1,16 @@
+//Write a function, "undirectedPath", that takes in an array of edges for an undirected graph and two nodes(nodeA, nodeB). The function should return a boolean indicating whether or not there exist a path between nodeA and nodeB.
+
+const edges = [
+    ['i', 'j'],
+    ['k', 'i'],
+    ['m', 'k'],
+    ['k', 'l'],
+    ['o', 'n']
+];
+
+const undirectedPath = (edges, nodeA, nodeB) => {
+
+}
+
+console.log(undirectedPath(edges, "j", "m"))
+  
