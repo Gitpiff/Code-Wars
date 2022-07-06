c1d408fa66565756688d97b42bb740677795720c
Victor <64720348+Gitpiff@users.noreply.github.com>
2022-07-05 21:34:05 -0500

    7.- Shortest-path

---
+++ structy/graphs/7.- Shortest-path.js
@@ -0,0 +1,5 @@
+//Write a function, "shortestPath", that takes in an array of edges for an undirected path and two nodes (nodeA, nodeB). The function should return the length of the shortest path between A and B. Consider the length as the number of edges in the path, not the number of nodes. If there's no path between A and B, then return -1.
+
+const shortestPath = (edges, nodeA, nodeB) => {
+    
+}
