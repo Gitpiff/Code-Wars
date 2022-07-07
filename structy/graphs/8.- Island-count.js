90a7c980e73260471b4a0429d38fecbb0aeb9eb1
Victor <64720348+Gitpiff@users.noreply.github.com>
2022-07-07 13:44:19 -0500

    Create 8.- Island-count.js

---
+++ structy/graphs/8.- Island-count.js
@@ -0,0 +1,17 @@
+//Write a function, "islandCount", that takes in a grid containing Ws and Ls. W represents water and L represents Land. The function shoudl return the number of islands on the grid. An island is a vertically or horizontally connected region of land.
+
+const islandCount = (w, l) => {
+
+}
+
+const grid = [
+    ['W', 'L', 'W', 'W', 'W'],
+    ['W', 'L', 'W', 'W', 'W'],
+    ['W', 'W', 'W', 'L', 'W'],
+    ['W', 'W', 'L', 'L', 'W'],
+    ['L', 'W', 'W', 'L', 'L'],
+    ['L', 'L', 'W', 'W', 'W'],
+  ];
+  
+
+console.log(islandCount(grid))
