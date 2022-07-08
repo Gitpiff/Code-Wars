39072596039ac6593969ded5fd1d51f2b52ed4bf
Victor <64720348+Gitpiff@users.noreply.github.com>
2022-07-07 19:20:56 -0500

    Update 8.- Island-count.js

--- structy/graphs/8.- Island-count.js
+++ structy/graphs/8.- Island-count.js
@@ -4,22 +4,26 @@ const islandCount = (grid) => {
     //the positions -coordinates- of the graph will act like nodes
     //avoid inifinte loops, use a set
     const visited = new Set();
+    //set a count var
+    let count = 0;
     //start with a loop to iterate through every position in the graph, we're looking for rows and columns
     for (let r = 0; r < grid.length; r += 1){
         for (let c = 0; c < grid[0].length; c += 1){
-            //invoke a helper function to do the traversal
-            explore(grid, c, r, visited);
+            //invoke a helper function to do the traversal and check if the return of the helper call is true, add 1 to the count
+            if (explore(grid, r, c, visited) === true) {
+                count += 1;
+            }
         }
     }
 
-
+    return count;
 }
 
 //helper function
-const explore = (grid, c, r, visited) => {
+const explore = (grid, r, c, visited) => {
     //make sure our positions are inbounds
     const rowInbounds = 0 <= r && r < grid.length;
-    const colInbounds = 0 <= c && c < grid.length;
+    const colInbounds = 0 <= c && c < grid[0].length;
     if (!rowInbounds || !colInbounds) return false;
     //if the position is water, return false
     if (grid[r][c] === "W") return false; 
@@ -27,18 +31,24 @@ const explore = (grid, c, r, visited) => {
     //we suse a commma because we need to set different bounds for c and r
     const position = r + "," + c;
     //check if the island has been visited
-    if (visited.has(pos)) return false;
-    visited.add(pos);
+    if (visited.has(position)) return false;
+    visited.add(position);
+
+    //if we want to go above in the grid, r -1
+    explore(grid, r - 1, c, visited); //up
+    explore(grid, r + 1, c, visited); //down
+    explore(grid, r, c -1, visited); //left
+    explore(grid, r, c + 1, visited); //right
+
+    return true; //we found a new island
 }
 
 const grid = [
-    ['W', 'L', 'W', 'W', 'W'],
-    ['W', 'L', 'W', 'W', 'W'],
-    ['W', 'W', 'W', 'L', 'W'],
-    ['W', 'W', 'L', 'L', 'W'],
+    ['L', 'W', 'W', 'L', 'W'],
     ['L', 'W', 'W', 'L', 'L'],
-    ['L', 'L', 'W', 'W', 'W'],
+    ['W', 'L', 'W', 'L', 'W'],
+    ['W', 'W', 'W', 'W', 'W'],
+    ['W', 'W', 'L', 'L', 'L'],
   ];
-  
 
 console.log(islandCount(grid))
