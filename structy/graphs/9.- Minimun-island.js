cf4b0cccc735ddc6be4df16a59fb961aa7cc4190
Victor <64720348+Gitpiff@users.noreply.github.com>
2022-07-07 19:29:01 -0500

    Create 9.- Minimun-island.js

---
+++ structy/graphs/9.- Minimun-island.js
@@ -0,0 +1,17 @@
+//Write a function, "minimumIsland", that takes in a grid containing Ws and Ls. W represents water, L represents land. The function should return the size of the smalles island.  An island is a vertically or horizontally connected region of land. You may assume that the grid contains at least one island.
+
+const minimumIsland = (grid) => {
+
+}
+
+const grid = [
+    ['W', 'L', 'W', 'W', 'W'],
+    ['W', 'L', 'W', 'W', 'W'],
+    ['W', 'W', 'W', 'L', 'W'],
+    ['W', 'W', 'L', 'L', 'W'],
+    ['L', 'W', 'W', 'L', 'L'],
+    ['L', 'L', 'W', 'W', 'W'],
+  ];
+
+  
+console.log(minimumIsland(grid))
