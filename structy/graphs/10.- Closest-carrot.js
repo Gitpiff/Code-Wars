378ded6432f85821cc0bcb7ea53e69e2ec374bb0
Victor <64720348+Gitpiff@users.noreply.github.com>
2022-07-09 21:17:45 -0500

    Update 10.- Closest-carrot.js

--- structy/graphs/10.- Closest-carrot.js
+++ structy/graphs/10.- Closest-carrot.js
@@ -1,6 +1,30 @@
 // Write a function, closestCarrot, that takes in a grid, a starting row, and a starting column. In the grid, 'X's are walls, 'O's are open spaces, and 'C's are carrots. The function should return a number representing the length of the shortest path from the starting position to a carrot. You may move up, down, left, or right, but cannot pass through walls (X). If there is no possible path to a carrot, then return -1.
 
 const closestCarrot = (grid, startRow, startCol) => {
+    const queue = [ [startRow, startCol, 0] ];
+    const visited = new Set([ startRow + ',' + startCol ]);
+  
+    while (queue.length > 0) {
+        const [ row, col, distance ] = queue.shift();
+    
+        if (grid[row][col] === 'C') return distance;
+    
+        const deltas = [[1, 0], [-1, 0], [0, 1], [0, -1]];
+        for (let delta of deltas)  {
+        const [deltaRow, deltaCol] = delta;
+        const neighborRow = row + deltaRow;
+        const neighborCol = col + deltaCol;
+        const neighborPos = neighborRow + ',' + neighborCol;
+        const rowInbounds = 0 <= neighborRow && neighborRow < grid.length;
+        const colInbounds = 0 <= neighborCol && neighborCol < grid[0].length;
+        if (rowInbounds && colInbounds && !visited.has(neighborPos) && grid[neighborRow][neighborCol] !== 'X') {
+            visited.add(neighborPos);
+            queue.push([ neighborRow, neighborCol, distance + 1 ]);
+      }
+    }
+  }
+  
+  return -1;
 
 };
 
