ff80daf6aa2bf492337eda0f83801862c3f95a26
Victor <64720348+Gitpiff@users.noreply.github.com>
2022-07-09 21:16:00 -0500

    Create 10.- Closest-carrot.js

---
+++ structy/graphs/10.- Closest-carrot.js
@@ -0,0 +1,17 @@
+// Write a function, closestCarrot, that takes in a grid, a starting row, and a starting column. In the grid, 'X's are walls, 'O's are open spaces, and 'C's are carrots. The function should return a number representing the length of the shortest path from the starting position to a carrot. You may move up, down, left, or right, but cannot pass through walls (X). If there is no possible path to a carrot, then return -1.
+
+const closestCarrot = (grid, startRow, startCol) => {
+
+};
+
+const grid = [
+    ['O', 'O', 'O', 'O', 'O'],
+    ['O', 'X', 'O', 'O', 'O'],
+    ['O', 'X', 'X', 'O', 'O'],
+    ['O', 'X', 'C', 'O', 'O'],
+    ['O', 'X', 'X', 'O', 'O'],
+    ['C', 'O', 'O', 'O', 'O'],
+];
+
+console.log(closestCarrot(grid, 0, 0))
+
