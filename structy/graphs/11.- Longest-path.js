25d165cd57c70b9c262dfc350504032c53c013c9
Victor <64720348+Gitpiff@users.noreply.github.com>
2022-07-10 19:10:04 -0500

    Create 11.- Longest-path.js

---
+++ structy/graphs/11.- Longest-path.js
@@ -0,0 +1,14 @@
+//Write a function, "longesPath" that takes in an adjancency list for a directed acyclic graph. The function should return the length of the longest path within the graph. A path may start and end at any two nodes. The lenght of a path is considered the number of edges in the path not the number of nodes.
+
+const longesPath = (graph) => {
+
+};
+
+const graph = {
+    a: ['c', 'b'],
+    b: ['c'],
+    c: []
+  };
+  
+
+console.log(longesPath(graph))
