2c031a9975b1b78869b5ee53d1873350a7df9014
Victor <viknavarro@gmail.com>
2022-07-15 11:33:22 -0500

    Create mergeSort.js

---
+++ structy/mergeSort.js
@@ -0,0 +1,14 @@
+//Write a function, "mergeSort", that takes in an array of numbers as an argument. The function should return a new array containing elements of the original array sorted in ascending order. Your function must implement the merge sort algorithm.
+
+
+//Divide an conquer, make sure to use MAth.floor when dividing so we always get a whole number
+//divide the array
+//store in 2 var the left side and right side of it
+//we'll need a helper function to help us merge
+
+
+const mergeSort = (nums) => {
+
+};
+
+console.log(mergeSort([10, 4, 42, 5, 8, 100, 5, 6, 12, 40]));
