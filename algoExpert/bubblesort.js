ca19b342497b3f119c9b09adfb37cd8440fd05d3
Victor <64720348+Gitpiff@users.noreply.github.com>
2022-07-14 19:21:12 -0500

    create bubblesort

---
+++ algoExpert/bubblesort.js
@@ -0,0 +1,8 @@
+//Given two non empty arrays of integers, write a function that determines whether the second array is a subsequence of the first one.
+//A subsequence of an array is a set of numbers that aren't neccessarily adjacent in the array but that are in the same order as they appear in the array. For instance the numbers [1, 3, 4] form a subsequence of the array [1, 2, 3, 4, 5], and so do the numbers [2, 4]. Note that a single number in an array and the array itself are both valid subsequences of the array.
+
+const isValidSubsequence = (array, subsequence) => {
+
+};
+
+console.log(isValidSubsequence([5, 1, 22, 25, 6, -1, 8, 10],[1, 6, -1, 10]));
