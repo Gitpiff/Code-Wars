c515a8ade6580e2f7e40a9f9e8533fd5f93e672d
Victor <viknavarro@gmail.com>
2022-07-15 13:36:53 -0500

    Update mergeSort.js

--- structy/mergeSort.js
+++ structy/mergeSort.js
@@ -8,7 +8,48 @@
 
 
 const mergeSort = (nums) => {
+    //if the array contains 0 or 1 element then its already sorted
+    if (nums.length <= 1) return nums;
+
+    //split array in half -make sure to get a whole num-
+    const mid = Math.floor(nums.length / 2);
+
+    //get the two halves of the array and make the recursive call
+    const leftSide = mergeSort(nums.slice(0, mid));
+    const rightSide = mergeSort(nums.slice(mid));
+
+    //call the helper merge function
+    return merge(leftSide, rightSide);
 
 };
 
-console.log(mergeSort([10, 4, 42, 5, 8, 100, 5, 6, 12, 40]));
+//helper function, get both sides of the array as arguments
+const merge = (nums1, nums2) => {
+    //reverse the given arrays so we can avoid using .shift() to get the smallest element, by reversing them we get the smallest element from the back of the array
+    nums1.reverse();
+    nums2.reverse();
+    //declare a merged array to store the sorted elements
+    const merged = [];
+
+    //iterate through both arguments while they are not empty
+    while (nums1.length > 0 && nums2.length > 0) {
+        //since we reversed the arrays, check from the back of the array, if nums1 is less than nums2, .pop() it into merged
+        if (nums1[nums1.length - 1] < nums2[nums2.length - 1]) {
+            merged.push(nums1.pop());
+        }
+        //else push nums2
+        else {
+            merged.push(nums2.pop());
+        }
+    }
+
+    //after the while loop is done, one of the two arrays will have some elements left behind, take them and push them to merged, make sure to reverse them to their original state before pushing them
+    merged.push(...nums1.reverse());
+    merged.push(...nums2.reverse());
+
+    //return the merged array
+    return merged;
+}
+
+const numbers = new Array(120000).fill(7);
+console.log(mergeSort(numbers));
