581eecac977383913e1122fc11b39b20551f0ad5
Victor <viknavarro@gmail.com>
2022-07-16 16:48:07 -0500

    Create 14.- Has-cycle.js

---
+++ structy/graphs/14.- Has-cycle.js
@@ -0,0 +1,11 @@
+//Write a function, "hasCycle", that takes in an object representing the adjacency list of a directed graph. The function should return a boolean indicating whether or not the graph contains a cycle.
+
+const hasCycle = (graph) => {
+
+};
+
+console.log(hasCycle({
+    a: ["b"],
+    b: ["c"],
+    c: ["a"],
+  }));
