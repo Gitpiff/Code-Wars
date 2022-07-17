a849787032358fd84ff11f9464b9d764584ff73c
Victor <64720348+Gitpiff@users.noreply.github.com>
2022-07-17 16:51:27 -0500

    Create 15.- Prereqs-possible.js

---
+++ structy/graphs/15.- Prereqs-possible.js
@@ -0,0 +1,17 @@
+// Write a function, prereqsPossible, that takes in a number of courses (n) and prerequisites as arguments. Courses have ids ranging from 0 through n - 1. A single prerequisite of [A, B] means that course A must be taken before course B. The function should return a boolean indicating whether or not it is possible to complete all courses.
+
+const prereqsPossible = (numCourses, prereqs) => {
+    // todo
+};
+
+
+const numCourses = 6;
+const prereqs = [
+  [0, 1],
+  [2, 3],
+  [0, 2],
+  [1, 3],
+  [4, 5],
+];
+
+console.log(prereqsPossible(numCourses, prereqs))
