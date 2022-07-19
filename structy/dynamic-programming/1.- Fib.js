dc5342f7860424393e428166111ddf8ea64a09fe
Victor <64720348+Gitpiff@users.noreply.github.com>
2022-07-19 18:57:49 -0500

    Update 1.- Fib.js

--- structy/dynamic-programming/1.- Fib.js
+++ structy/dynamic-programming/1.- Fib.js
@@ -8,15 +8,25 @@
 
 // Solve this recursively.
 
-const fib = (n) => {
+// const fib = (n) => {
 
+//   if (n === 0 || n === 1) return n;
+
+//   return fib(n - 1) + fib(n - 2);
+// };
+//Time: O(2^n)
+//Space: O(n)
+
+//memoized (pass)
+const fib = (n, memo = {}) => {
   if (n === 0 || n === 1) return n;
 
-  return fib(n - 1) + fib(n - 2);
+  if (n in memo) return memo[n];
+
+  memo[n] = fib(n - 1, memo) + fib(n - 2, memo);
+  return memo[n];
 };
-
-
-//Time: O(2^n)
+//Time: O(n)
 //Space: O(n)
 
 
