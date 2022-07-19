7aa03cc03f0d6dcff8ef0b10ccc99f86702bd190
Victor <64720348+Gitpiff@users.noreply.github.com>
2022-07-18 20:40:41 -0500

    Create 1.- Fib.js

---
+++ structy/dynamic-programming/1.- Fib.js
@@ -0,0 +1,15 @@
+// Write a function fib that takes in a number argument, n, and returns the n-th number of the Fibonacci sequence.
+
+// The 0-th number of the sequence is 0.
+
+// The 1-st number of the sequence is 1.
+
+// To generate further numbers of the sequence, calculate the sum of previous two numbers.
+
+// Solve this recursively.
+
+const fib = (n) => {
+    // todo
+};
+
+console.log(fib(35))
