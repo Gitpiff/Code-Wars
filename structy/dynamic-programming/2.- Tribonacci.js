cbfffe45578f3237ff13d5e9e783052e82f3f3ba
Victor <64720348+Gitpiff@users.noreply.github.com>
2022-07-20 19:09:16 -0500

    Create 2.- Tribonacci.js

---
+++ structy/dynamic-programming/2.- Tribonacci.js
@@ -0,0 +1,15 @@
+// Write a function tribonacci that takes in a number argument, n, and returns the n-th number of the Tribonacci sequence.
+
+// The 0-th and 1-st numbers of the sequence are both 0.
+
+// The 2-nd number of the sequence is 1.
+
+// To generate further numbers of the sequence, calculate the sum of previous three numbers.
+
+// Solve this recursively.
+
+const tribonacci = (n) => {
+    
+};
+
+console.log(tribonacci(37));
