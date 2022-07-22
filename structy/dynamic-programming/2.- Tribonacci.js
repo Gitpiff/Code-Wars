0bde9454427abeec664e90b86fccba86baca5581
Victor <viknavarro@gmail.com>
2022-07-21 20:01:33 -0500

    Update 2.- Tribonacci.js

--- structy/dynamic-programming/2.- Tribonacci.js
+++ structy/dynamic-programming/2.- Tribonacci.js
@@ -9,16 +9,30 @@
 // Solve this recursively.
 
 //brute-force (timeout)
+// const tribonacci = (n) => {
+//   if (n === 0 || n === 1) return 0;
 
-const tribonacci = (n) => {
+//   if (n === 2) return 1;
+
+//   return tribonacci(n - 1) + tribonacci(n - 2) + tribonacci(n - 3);
+// };
+
+//Time: O(3^n)
+//Space: O(n)
+
+//memoized (pass)
+const tribonacci = (n, memo = {}) => {
+  if (n in memo) return memo[n];
+
   if (n === 0 || n === 1) return 0;
 
   if (n === 2) return 1;
 
-  return tribonacci(n - 1) + tribonacci(n - 2) + tribonacci(n - 3);
+  memo[n] = tribonacci(n - 1, memo) + tribonacci(n - 2, memo) + tribonacci(n - 3, memo);
+  return memo[n];
 };
-//Time: O(3^n)
-//Space: O(n)
 
+// Time: O(n)
+// Space: O(n)
 
 console.log(tribonacci(37));
