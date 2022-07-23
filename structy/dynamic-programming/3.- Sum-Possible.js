a76749ea1af8f7ed4b0fc3cca81e597d8bace2d9
Victor <viknavarro@gmail.com>
2022-07-22 19:57:00 -0500

    Create 3.- Sum-Possible.js

---
+++ structy/dynamic-programming/3.- Sum-Possible.js
@@ -0,0 +1,10 @@
+// Write a function sumPossible that takes in an amount and an array of positive numbers. The function should return a boolean indicating whether or not it is possible to create the amount by summing numbers of the array. You may reuse numbers of the array as many times as necessary.
+
+// You may assume that the target amount is non-negative.
+
+const sumPossible = (amount, numbers) => {
+    // todo
+};
+
+
+console.log(sumPossible(8, [5, 12, 4]))
