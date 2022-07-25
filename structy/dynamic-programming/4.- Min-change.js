0ed13998fbbd1bacc346cf5f3e4f19787a64dea1
Victor <viknavarro@gmail.com>
2022-07-24 19:07:04 -0500

    Update 4.- Min-change.js

--- structy/dynamic-programming/4.- Min-change.js
+++ structy/dynamic-programming/4.- Min-change.js
@@ -3,7 +3,29 @@
 // If it is not possible to create the amount, then return -1.
 
 const minChange = (amount, coins) => {
-    // todo
+    // dynamic programming with memoization
+    const answer = _minChange(amount, coins);
+    return answer === Infinity ? -1 : answer;
 };
+
+const _minChange = (amount, coins, memo = {}) => {
+    if (amount < 0) return Infinity;
+
+    if (amount === 0) return 0;
+
+    if (amount in memo) return memo[amount];
+
+    let min = Infinity;
+    for (let coin of coins) {
+      const numCoins = 1 + _minChange(amount - coin, coins, memo);
+      min = Math.min(numCoins, min);
+    }
+    return memo[amount] = min;
+};
+
+//a = amount
+//c = # coins
+//Time: O(a*c)
+//Space: O(a)
 
 console.log(13, [1, 9, 5, 14, 30]);
