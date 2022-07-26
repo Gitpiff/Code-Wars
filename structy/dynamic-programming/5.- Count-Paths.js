df18b12981c09d4fe1c22e9a0671d0830d883821
Victor <viknavarro@gmail.com>
2022-07-25 19:25:05 -0500

    Update 5.- Count-Paths.js

--- structy/dynamic-programming/5.- Count-Paths.js
+++ structy/dynamic-programming/5.- Count-Paths.js
@@ -1,7 +1,16 @@
 // Write a function, countPaths, that takes in a grid as an argument. In the grid, 'X' represents walls and 'O' represents open spaces. You may only move down or to the right and cannot pass through walls. The function should return the number of ways possible to travel from the top-left corner of the grid to the bottom-right corner.
 
-const countPaths = (grid) => {
-    // todo
+//dynamic programming with memoization
+const countPaths = (grid, r = 0, c = 0, memo = {}) => {
+    const pos = r + ',' + c;
+  if (pos in memo) return memo[pos];
+
+  if (r === grid.length || c === grid[0].length || grid[r][c] === 'X') return 0;
+
+  if (r === grid.length - 1 && c === grid[0].length - 1) return 1;
+
+  memo[pos] = countPaths(grid, r + 1, c, memo) + countPaths(grid, r, c + 1, memo);
+  return memo[pos];
 };
 
 const grid = [
@@ -21,6 +30,11 @@ const grid = [
     ["O", "O", "O", "O", "O", "O", "O", "O", "X", "O", "O", "O", "O", "O", "O"],
     ["O", "O", "O", "O", "O", "O", "O", "O", "X", "O", "O", "O", "O", "O", "O"],
   ];
+
+  //r = # rows
+  //c = # columns
+  //Time: O(r*c)
+  //Space: O(r*c)
 
 console.log(countPaths(grid))
 
