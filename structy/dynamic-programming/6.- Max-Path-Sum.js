c5d64e3a2792b18c1993cf9a303d2e69614f524e
Victor <64720348+Gitpiff@users.noreply.github.com>
2022-08-01 20:59:57 -0500

    Create 6.- Max-Path-Sum.js

---
+++ structy/dynamic-programming/6.- Max-Path-Sum.js
@@ -0,0 +1,15 @@
+// Write a function, maxPathSum, that takes in a grid as an argument. The function should return the maximum sum possible by traveling a path from the top-left corner to the bottom-right corner. You may only travel through the grid by moving down or right.
+
+// You can assume that all numbers are non-negative.
+
+const maxPathSum = (grid) => {
+    // todo
+};
+
+const grid = [
+    [1, 3, 12],
+    [5, 1, 1],
+    [3, 6, 1],
+];
+
+console.log(maxPathSum(grid));
