db4210de92764a4ee4b75f483978c07199fe6dca
Victor <64720348+Gitpiff@users.noreply.github.com>
2022-08-01 21:02:15 -0500

    Update 6.- Max-Path-Sum.js

--- structy/dynamic-programming/6.- Max-Path-Sum.js
+++ structy/dynamic-programming/6.- Max-Path-Sum.js
@@ -2,8 +2,21 @@
 
 // You can assume that all numbers are non-negative.
 
-const maxPathSum = (grid) => {
-    // todo
+//dynamic programming with memoization
+const maxPathSum = (grid, r = 0, c = 0, memo = {}) => {
+    const pos = r + ',' + c;
+    if (pos in memo) return memo[pos];
+    
+    if (r === grid.length || c === grid[0].length) return -Infinity;
+    
+    
+    if (r === grid.length - 1 && c === grid[0].length - 1) return grid[r][c];
+    
+    const down = maxPathSum(grid, r + 1, c, memo);
+    const right = maxPathSum(grid, r, c + 1, memo);
+        
+    memo[pos] = grid[r][c] + Math.max(down, right);
+    return memo[pos];
 };
 
 const grid = [
