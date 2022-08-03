21db95241c6a28f65906b6080b7510a31e86f19d
Victor <64720348+Gitpiff@users.noreply.github.com>
2022-08-02 23:24:31 -0500

    Create 7.- Non-Adjacent.js

---
+++ structy/dynamic-programming/7.- Non-Adjacent.js
@@ -0,0 +1,8 @@
+// Write a function, nonAdjacentSum, that takes in an array of numbers as an argument. The function should return the maximum sum of non-adjacent elements in the array. There is no limit on how many elements can be taken into the sum as long as they are not adjacent.
+
+const nonAdjacentSum = (nums) => {
+    
+};
+
+const nums = [2, 4, 5, 12, 7];
+console.log(nonAdjacentSum(nums));
