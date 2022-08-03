279edbe3b73d4f612da8504a39699bca0d3c5fee
Victor <64720348+Gitpiff@users.noreply.github.com>
2022-08-02 23:29:54 -0500

    Update 7.- Non-Adjacent.js

--- structy/dynamic-programming/7.- Non-Adjacent.js
+++ structy/dynamic-programming/7.- Non-Adjacent.js
@@ -1,8 +1,28 @@
 // Write a function, nonAdjacentSum, that takes in an array of numbers as an argument. The function should return the maximum sum of non-adjacent elements in the array. There is no limit on how many elements can be taken into the sum as long as they are not adjacent.
 
-const nonAdjacentSum = (nums) => {
-    
+// For example, given:
+
+// [2, 4, 5, 12, 7]
+
+// The maximum non-adjacent sum is 16, because 4 + 12. 
+// 4 and 12 are not adjacent in the array.
+
+
+//dynamic programming with memoization
+const nonAdjacentSum = (nums, i = 0, memo = {}) => {
+    if (i in memo) return memo[i];
+  
+  if (i >= nums.length) return 0;
+  
+  const include = nums[i] + nonAdjacentSum(nums, i + 2, memo);
+  const exclude = nonAdjacentSum(nums, i + 1, memo);
+  memo[i] = Math.max(include, exclude);
+  return memo[i]
 };
+
+//n = length of nums
+//Time: O(n)
+//Space: O(n)
 
 const nums = [2, 4, 5, 12, 7];
 console.log(nonAdjacentSum(nums));
