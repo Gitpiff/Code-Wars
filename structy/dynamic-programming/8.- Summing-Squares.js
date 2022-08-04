3e463d955d6a3a4d5593f0d310a0dc9db26da472
Victor <viknavarro@gmail.com>
2022-08-03 21:05:47 -0500

    Create 8.- Summing-Squares.js

---
+++ structy/dynamic-programming/8.- Summing-Squares.js
@@ -0,0 +1,18 @@
+// Write a function, summingSquares, that takes a target number as an argument. The function should return the minimum number of perfect squares that sum to the target. A perfect square is a number of the form (i*i) where i >= 1.
+
+// For example: 1, 4, 9, 16 are perfect squares, but 8 is not perfect square.
+
+// Given 12:
+
+// summingSquares(12) -> 3
+
+// The minimum squares required for 12 is three, by doing 4 + 4 + 4.
+
+// Another way to make 12 is 9 + 1 + 1 + 1, but that requires four perfect squares.
+
+
+const summingSquares = (n) => {
+    // todo
+};
+
+console.log(summingSquares(8));
