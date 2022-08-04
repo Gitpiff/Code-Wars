3c19fa565679c74193e3e54328b9fbf2547f857a
Victor <viknavarro@gmail.com>
2022-08-03 21:07:06 -0500

    Update 8.- Summing-Squares.js

--- structy/dynamic-programming/8.- Summing-Squares.js
+++ structy/dynamic-programming/8.- Summing-Squares.js
@@ -10,9 +10,21 @@
 
 // Another way to make 12 is 9 + 1 + 1 + 1, but that requires four perfect squares.
 
+//dynamic programming with memoization
+const summingSquares = (n, memo = {}) => {
+    if (n in memo) return memo[n];
 
-const summingSquares = (n) => {
-    // todo
+    if (n === 0) return 0;
+
+  let minSquares = Infinity;
+  for (let i = 1; i <= Math.sqrt(n); i += 1) {
+    const square = i * i;
+    const numSquares = 1 + summingSquares(n - square, memo);
+    minSquares = Math.min(minSquares, numSquares);
+  }
+
+  memo[n] = minSquares;
+  return minSquares;
 };
 
 console.log(summingSquares(8));
