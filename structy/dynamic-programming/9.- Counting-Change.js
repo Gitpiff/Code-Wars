77e1791d9283ba1f829fbd891bb5a29b3242a766
Victor <64720348+Gitpiff@users.noreply.github.com>
2022-08-05 11:18:21 -0500

    Update 9.- Counting-Change.js

--- structy/dynamic-programming/9.- Counting-Change.js
+++ structy/dynamic-programming/9.- Counting-Change.js
@@ -13,8 +13,25 @@
 // 3. 1 + 3
 // 4. 2 + 2
 
-const countingChange = (amount, coins) => {
-   
+//dynamic programming with memoization
+
+const countingChange = (amount, coins, i = 0, memo = {}) => {
+    const key = amount + ',' + i;
+    if (key in memo) return memo[key];
+    
+    if (amount === 0) return 1;
+    if (i === coins.length) return 0;
+      
+    const coin = coins[i];
+    
+    let count = 0;
+    for (let qty = 0; (qty * coin) <= amount; qty += 1) {
+      const remainder = amount - (coin * qty);
+      count += countingChange(remainder, coins, i + 1, memo);
+    }
+    
+    memo[key] = count;
+    return count;
 };
 
 console.log(countingChange(4, [1, 2, 3]));
