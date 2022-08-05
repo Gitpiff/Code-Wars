c60406787a2d861389ede92af0f91063ffb67911
Victor <64720348+Gitpiff@users.noreply.github.com>
2022-08-05 11:16:39 -0500

    Create 9.- Counting-Change.js

---
+++ structy/dynamic-programming/9.- Counting-Change.js
@@ -0,0 +1,20 @@
+// Write a function, countingChange, that takes in an amount and an array of coins. The function should return the number of different ways it is possible to make change for the given amount using the coins.
+
+// You may reuse a coin as many times as necessary.
+
+// For example,
+
+// countingChange(4, [1,2,3]) -> 4
+
+// There are four different ways to make an amount of 4:
+
+// 1. 1 + 1 + 1 + 1
+// 2. 1 + 1 + 2
+// 3. 1 + 3
+// 4. 2 + 2
+
+const countingChange = (amount, coins) => {
+   
+};
+
+console.log(countingChange(4, [1, 2, 3]));
